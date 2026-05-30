292e5df5ab25176e13e60054422d04de7c2f1b3b
MAltizer96 <Maltizer96@gmail.com>
2026-05-30 10:37:29 -0400

    implemented env file

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from main_selenium import open_webpage
+import main_selenium
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+BUY_HOME_PAGE = os.getenv("BUY_HOME_PAGE")
+CITYS = os.getenv("CITYS").split(",")
+SEARCH_BOX_LABEL = os.getenv("SEARCH_BOX_LABEL")
+VERIFICATION_PRESS_HOLD_BUTTON = os.getenv("VERIFICATION_PRESS_HOLD_BUTTON")
+
 def main():
-    open_webpage("https://www.google.com")
+    main_selenium_OB = main_selenium.Main_Selenium()
+    main_selenium_OB.open_webpage(BUY_HOME_PAGE)
+    main_selenium_OB.check_element_exists("label", SEARCH_BOX_LABEL)
     print("Hello, World!")
 
 if __name__ == "__main__":
